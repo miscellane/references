da1bc37a43d8cbac76ccf24577b6e04a03f3828a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-19 18:39:31 +0100

    Update fundamentals/events/src/main/scala/com/grey/events/DataSteps.scala

--- fundamentals/events/src/main/scala/com/grey/events/DataSteps.scala
+++ fundamentals/events/src/main/scala/com/grey/events/DataSteps.scala
@@ -2,21 +2,27 @@ package com.grey.events
 
 import org.apache.spark.sql.SparkSession
 
+/**
+ *
+ * @param spark: A instance of SparkSession
+ */
 class DataSteps(spark: SparkSession) {
 
+  /**
+   *
+   */
   def dataSteps(): Unit = {
+
+    /**
+     * val ablyBitCoin = new AblyBitCoin()
+     * ablyBitCoin.ablyBitCoin()
+     *
+     * val ablyTube = new AblyTube()
+     * ablyTube.ablyTube()
+     */
 
     val ablyWeather = new AblyWeather(spark)
     ablyWeather.ablyWeather()
-
-    // val ablyWeatherEvents = new AblyWeatherEvents(spark)
-    // ablyWeatherEvents.ablyWeatherEvents()
-
-    // val ablyBitCoin = new AblyBitCoin()
-    // ablyBitCoin.ablyBitCoin()
-
-    // val ablyTube = new AblyTube()
-    // ablyTube.ablyTube()
 
   }
 
